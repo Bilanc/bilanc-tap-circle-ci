1fdbf3627add9471f76d30f350db12353205a375
Leyu <lambert@bilanc.co>
2026-01-07 14:28:16 +0000

    add function to get all projects

--- tap_circle_ci/sync.py
+++ tap_circle_ci/sync.py
@@ -1,4 +1,5 @@
 """tap-circle-ci sync."""
+
 from typing import Dict
 
 import singer
@@ -9,10 +10,38 @@ from tap_circle_ci.streams import STREAMS
 LOGGER = singer.get_logger()
 
 
-def sync(config :dict, state: Dict, catalog: singer.Catalog):
+def get_all_projects(client: Client) -> list:
+    """Fetches all projects available in the CircleCI account."""
+    all_projects = []
+    params = {}
+    response = client.get(
+        endpoint="https://circleci.com/api/v1.1/projects",
+        params=params,
+        headers={},
+    )
+    for project in response:
+        project_id: str = project["vcs_url"].split("/")[-1]
+        response = client.get(
+            endpoint=f"https://circleci.com/api/v2/project/{project_id}",
+            params={},
+            headers={},
+        )
+        project_slug: str = response.get("slug")
+        if not project_slug:
+            LOGGER.warning("Could not fetch slug for project ID: %s", project_id)
+            continue
+        all_projects.append(project_slug)
+    return all_projects
+
+
+def sync(config: dict, state: Dict, catalog: singer.Catalog):
     """performs sync for selected streams."""
     client = Client(config)
-    projects = list(filter(None, client.config["project_slugs"].split(" ")))
+    projects = (
+        list(filter(None, client.config["project_slugs"].split(" ")))
+        if config.get("project_slugs")
+        else get_all_projects(client)
+    )
     with singer.Transformer() as transformer:
         for stream in catalog.get_selected_streams(state):
             tap_stream_id = stream.tap_stream_id
